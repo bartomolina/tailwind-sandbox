7c96a404ffcdeed0e0b4945bba563677d1b35277
Barto Molina <bartomolina@gmail.com>
2022-12-30 00:23:22 +0100

    Testing

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import App2 from "./App2";
 import "./css/tailwind.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App2 />, document.getElementById("root"));
